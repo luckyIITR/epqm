60226351154cc08b408657aedc45e03bbfc5702a
luckyIITR <luckykushwaha56@gmail.com>
2022-08-12 14:28:03 +0530

    updated speakers

--- src/components/Committeecompo/committeecompo.js
+++ src/components/Committeecompo/committeecompo.js
@@ -1,14 +1,153 @@
 import React from "react";
 
+var adv_com = [
+  {
+    name: "T. V. Ramakrishnan",
+    insti: "Indian Institute of Science, Bangalore, India",
+  },
+  {
+    name: "Elbio Dagotto",
+    insti: "University of Tennesse & Oak Ridge National Laboratory, USA",
+  },
+  {
+    name: "D. D. Sarma",
+    insti: "Indian Institute of Science, Bangalore, India",
+  },
+  {
+    name: "G. Baskaran",
+    insti: "Institute of Mathematical Sciences, Chennai, India",
+  },
+  {
+    name: "E. V. Sampathkumaran",
+    insti: "Tata Institute of Fundamental Research, Mumbai, India",
+  },
+  {
+    name: "R. Ramesh",
+    insti: "University of California, Berkeley, USA",
+  },
+  {
+    name: "Christian Bernhard",
+    insti: "University of Fribourg, Switzerland",
+  },
+  {
+    name: "A. Taraphder",
+    insti: "Indian Institute of Technology, Kharagpur, India",
+  },
+  {
+    name: "S. M. Yusuf",
+    insti: "Bhabha Atomic Research Center (BARC) Mumbai",
+  },
+];
+
+var tech_com = [
+  {
+    name: "Debraj Choudhury",
+    insti: "Indian Institute of Technology, Kharagpur, India",
+  },
+  {
+    name: "Dr. Anil Jain",
+    insti: "Bhaba Atomic Research centre (BARC), Mumbai, India",
+  },
+  {
+    name: "Kaushik Sen",
+    insti: "Indian Institute of Technology, Delhi, India",
+  },
+  {
+    name: "N. Mohanta",
+    insti: "Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Vivek. K. Malik",
+    insti: "Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "T. Maitra",
+    insti: "Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Peayush K. Choubey",
+    insti: "Indian Institute of Technology, Roorkee, India",
+  },
+];
+
+var org_com = [
+  {
+    name: "G. D. Varma",
+    insti:
+      "Head, Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+    sufix: "(Chairman)",
+  },
+  {
+    name: "T. Maitra",
+    insti: "Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+    sufix: "(Convener)",
+  },
+  {
+    name: "Vivek K. Malik",
+    insti: "Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+    sufix: "(Convener)",
+  },
+  {
+    name: "N. Mohanta",
+    insti: "Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Peayush K. Choubey",
+    insti: "Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Hem C. Kandpal",
+    insti: "Dept. of Chemistry, Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Tapas K. Mandal",
+    insti: "Dept. of Chemistry, Indian Institute of Technology, Roorkee, India",
+  },
+  {
+    name: "Ramesh Chandra",
+    insti: "Institute Instrumentation Centre, IIT Roorkee, India",
+  },
+  {
+    name: "Himanshu Fulara",
+    insti: "Dept. of Physics, Indian Institute of Technology, Roorkee, India",
+  },
+];
+
+function SortNames(data) {
+  // for (let j = 0; j < data.length; j++) {
+  //   let splited = data[j].name.split(" ");
+  //   data[j]["last_name"] = splited[splited.length - 1];
+  // }
+
+  function compareObjects(object1, object2, key) {
+    const obj1 = object1[key].toUpperCase();
+    const obj2 = object2[key].toUpperCase();
+
+    if (obj1 < obj2) {
+      return -1;
+    }
+    if (obj1 > obj2) {
+      return 1;
+    }
+    return 0;
+  }
+
+  data.sort((book1, book2) => {
+    return compareObjects(book1, book2, "name");
+  });
+}
+
+SortNames(adv_com);
+SortNames(org_com);
+SortNames(tech_com);
+console.log(adv_com);
 function committeecompo() {
   return (
     <div className="container mb-5 pb-5">
       <p className="h2 pt-5 mb-5 font-weight-bold">Patron</p>
       <div className="row">
         <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. A. K. Chaturvedi
-          </h5>
+          <h5 className="card-title font-weight-bold">A. K. Chaturvedi</h5>
           <p>Director, Indian Institute of Technology, Roorkee, India</p>
         </div>
       </div>
@@ -16,161 +155,45 @@ function committeecompo() {
       {/* Advisory Committee */}
       <p className="h2 pt-5 mb-5 font-weight-bold">Advisory Committee</p>
       <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. T. V. Ramakrishnan
-          </h5>
-          <p>Indian Institute of Science, Bangalore, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Elbio Dagotto</h5>
-          <p>University of Tennesse & Oak Ridge National Laboratory, USA</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. D. D. Sarma</h5>
-          <p>Indian Institute of Science, Bangalore, India</p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. G. Baskaran</h5>
-          <p>Institute of Mathematical Sciences, Chennai, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. E. V. Sampathkumaran
-          </h5>
-          <p>Tata Institute of Fundamental Research, Mumbai, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. R. Ramesh</h5>
-          <p>University of California, Berkeley, USA</p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. Christian Bernhard
-          </h5>
-          <p>University of Fribourg, Switzerland</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. A. Taraphder</h5>
-          <p>Indian Institute of Technology, Kharagpur, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. S. M. Yusuf</h5>
-          <p>Bhabha Atomic Research Center (BARC) Mumbai</p>
-        </div>
+        {adv_com.map((value, index) => {
+          return (
+            <div className="col-sm-3 card mr-4 p-4 mt-4" key={index}>
+              <h5 className="card-title font-weight-bold">{value.name}</h5>
+              <p>{value.insti}</p>
+            </div>
+          );
+        })}
       </div>
 
       {/* Technical Committee */}
 
       <p className="h2 pt-5 mb-5 font-weight-bold">Technical Committee</p>
       <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. Debraj Choudhury
-          </h5>
-          <p>Indian Institute of Technology, Kharagpur, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Dr. Anil Jain</h5>
-          <p>Bhaba Atomic Research centre (BARC), Mumbai, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Kaushik Sen</h5>
-          <p>Indian Institute of Technology, Delhi, India</p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. N. Mohanta</h5>
-          <p>Indian Institute of Technology, Roorkee, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Vivek. K. Malik</h5>
-          <p>Indian Institute of Technology, Roorkee, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. T. Maitra</h5>
-          <p>Indian Institute of Technology, Roorkee, India</p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. Peayush K. Choubey
-          </h5>
-          <p>Indian Institute of Technology, Roorkee, India</p>
-        </div>
+        {tech_com.map((value, index) => {
+          return (
+            <div className="col-sm-3 card mr-4 p-4 mt-4" key={index}>
+              <h5 className="card-title font-weight-bold">{value.name}</h5>
+              <p>{value.insti}</p>
+            </div>
+          );
+        })}
       </div>
 
       {/* Organizing Committee */}
 
       <p className="h2 pt-5 mb-5 font-weight-bold">Organizing Committee</p>
       <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. G. D. Varma</h5>
-          <p>
-            Head, Dept. of Physics, Indian Institute of Technology, Roorkee,
-            India <span className="font-weight-bold">(Chairman)</span>
-          </p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. T. Maitra</h5>
-          <p>
-            Dept. of Physics, Indian Institute of Technology, Roorkee, India
-            <span className="font-weight-bold"> (Convener)</span>
-          </p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Vivek K. Malik</h5>
-          <p>
-            Dept. of Physics, Indian Institute of Technology, Roorkee, India
-            <span className="font-weight-bold"> (Co-convener)</span>
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. N. Mohanta</h5>
-          <p>
-            Dept. of Physics, Indian Institute of Technology, Roorkee, India
-          </p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">
-            Prof. Peayush K. Choubey
-          </h5>
-          <p>
-            Dept. of Physics, Indian Institute of Technology, Roorkee, India
-          </p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Hem C. Kandpal</h5>
-          <p>
-            Dept. of Chemistry, Indian Institute of Technology, Roorkee, India
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Tapas K. Mandal</h5>
-          <p>
-            Dept. of Chemistry, Indian Institute of Technology, Roorkee, India
-          </p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Ramesh Chandra</h5>
-          <p>Institute Instrumentation Centre, IIT Roorkee, India</p>
-        </div>
-        <div className="col-sm-3 card mr-4 p-4 mt-4">
-          <h5 className="card-title font-weight-bold">Prof. Himanshu Fulara</h5>
-          <p>
-            Dept. of Physics, Indian Institute of Technology, Roorkee, India
-          </p>
-        </div>
+        {org_com.map((value, index) => {
+          return (
+            <div className="col-sm-3 card mr-4 p-4 mt-4" key={index}>
+              <h5 className="card-title font-weight-bold">{value.name}</h5>
+              <p>
+                {value.insti} <br />
+                <span className="font-weight-bold">{value.sufix}</span>
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/Speakercompo/Speakercomp.js
+++ src/components/Speakercompo/Speakercomp.js
@@ -73,6 +73,64 @@ var speakers = [
     name: "S. M. Yusuf",
     affiliated: "Bhabha Atomic Research Center (BARC) Mumbai",
   },
+  {
+    name: "C. S. Yadav",
+    affiliated: "Indian Institute of Technology, Mandi, India",
+  },
+  {
+    name: "Brijesh Kumar",
+    affiliated: "Jawaharlal Nehru University, India",
+  },
+  {
+    name: "N.S. Vidhyadhiraja",
+    affiliated:
+      "Jawaharlal Nehru Centre For Advanced Scientific Research, India",
+  },
+  {
+    name: "Sumiran Pujari",
+    affiliated: "Indian Institute of Technology, Bombay, India",
+  },
+  {
+    name: "Surjeet Singh",
+    affiliated: "IISER Pune, India",
+  },
+  {
+    name: "Rajendra Dhaka",
+    affiliated: "Indian Institute of Technology, Delhi, India",
+  },
+  {
+    name: "Subhankar Bedanta",
+    affiliated: "NISER Bhubaneswar, India",
+  },
+  {
+    name: "T. V. Ramakrishnan",
+    affiliated: " IISc, India",
+  },
+  {
+    name: "S. J. Ray",
+    affiliated: "Indian Institute of Technology, Patna, India",
+  },
+
+  {
+    name: "Siddharth Lal",
+    affiliated: "IISER Kolkata, India",
+  },
+  {
+    name: "Pratap Raychaudhuri",
+    affiliated: "TIFR, India",
+  },
+  {
+    name: "Elbio Dagotto",
+    affiliated: "University of Tennessee, USA",
+  },
+  {
+    name: "Tamalika Banerjee",
+    affiliated: "University of Groningen",
+  },
+  {
+    name: "Saikat Ghosh",
+    affiliated: "Indian Institute of Technology, Kanpur, India",
+  },
 
   //   {
   //     name: "J C Seamus Davis",
@@ -82,7 +140,31 @@ var speakers = [
 // speakers.sort((a, b) => {
 //   return a.name.split(" ").slice(-1)[0] < b.name.split(" ").slice(-1)[0];
 // });
-console.log(speakers);
+function SortNames(data) {
+  // for (let j = 0; j < data.length; j++) {
+  //   let splited = data[j].name.split(" ");
+  //   data[j]["last_name"] = splited[splited.length - 1];
+  // }
+
+  function compareObjects(object1, object2, key) {
+    const obj1 = object1[key].toUpperCase();
+    const obj2 = object2[key].toUpperCase();
+
+    if (obj1 < obj2) {
+      return -1;
+    }
+    if (obj1 > obj2) {
+      return 1;
+    }
+    return 0;
+  }
+
+  data.sort((book1, book2) => {
+    return compareObjects(book1, book2, "name");
+  });
+}
+
+SortNames(speakers);
 function Speakercomp() {
   return (
     <div className="container mb-5 pd-5">
@@ -94,9 +176,9 @@ function Speakercomp() {
         {speakers.map((value, index) => {
           return (
             <div className="col-md-5 card p-3 m-2 m-md-4" key={index}>
-              <p className="font-weight-bold">
+              <h5 className="card-title font-weight-bold">
                 {value.name} <cite title="Source Title">{value.title}</cite>
-              </p>
+              </h5>
               <p>{value.affiliated}</p>
             </div>
           );
