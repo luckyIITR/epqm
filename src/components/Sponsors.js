8c035689c39a2cdc96dfd545189ec2f0303d1f3e
luckyIITR <luckykushwaha56@gmail.com>
2022-10-01 15:42:37 +0530

    logo updated

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -3,9 +3,11 @@ import Nav from "./Homecompo/Nav";
 import Logo from "./Homecompo/Logo";
 import logo from "./images/logo.png";
 import logo2 from "./images/serb_logo.png";
-
+import thermo from "./images/ThermoFisher.webp";
+import brns from "./images/BRNS.jpg";
+import oxford from "./images/oxford.png";
 import Footer from "./Homecompo/Footer";
-
+import ir from "./images/IR_Tech.png";
 function About() {
   return (
     <div>
@@ -21,9 +23,40 @@ function About() {
         </div>
         <br />
         <br />
+
+        <div className="row justify-content-around pt-5 mt-5">
+          <div className="col-md-6">
+            <img src={brns} alt="IITR" className="sponsor-logo" />
+          </div>
+        </div>
+        <br />
+        <br />
+
+        <div className="row justify-content-around pt-5 mt-5">
+          <div className="col-md-6">
+            <img src={oxford} alt="IITR" className="sponsor-logo" />
+          </div>
+        </div>
+        <br />
+        <br />
+
+        <div className="row justify-content-around pt-5 mt-5">
+          <div className="col-md-6">
+            <img src={thermo} alt="IITR" className="sponsor-logo" />
+          </div>
+        </div>
+        <br />
+        <br />
+
         <div className="row justify-content-around pt-5 mt-5">
           <div className="col-md-6">
             <img src={logo2} alt="IITR" className="sponsor-logo" />
+          </div>
+        </div>
+
+        <div className="row justify-content-around pt-5 mt-5">
+          <div className="col-md-6">
+            <img src={ir} alt="IITR" className="sponsor-logo" />
           </div>
         </div>
       </div>
